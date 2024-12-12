c32029ff5d22cbc91c29ad389dbb59b751b7fbd9
Roman <romanchenskii@gmail.com>
2024-12-12 18:30:53 +0300

    Изменен внешний вид сайта, добавлена стилистика по макету

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const fileInput = document.querySelector('.file__input');
+    const fileLabel = document.querySelector('.file__label');
+
+    fileInput.addEventListener('change', () => {
+        const fileName = fileInput.files.length > 0 ? fileInput.files[0].name : 'Выберите файл';
+        let fileN = fileName[0] + fileName[1] + fileName[2] + fileName[3] + fileName[4] + "...";
+
+        fileLabel.textContent = fileN;
+    });
+});
+
+
+
+
+
+
+
+
+
 const form = document.getElementById('upload-form');
 const responseDiv = document.querySelector(".respons");
 
@@ -32,8 +52,8 @@ form.addEventListener('submit', async (event) => {
 
         // Отображаем ссылку для скачивания обработанного файла
         responseDiv.innerHTML = `
-            <p>${result.message}</p>
-            <a href="${url}${result.file_url}" download target="_blank">Скачать обработанный файл</a>
+            <p class="respons__text">${result.message}</p>
+            <a class="respons__link link" href="${url}${result.file_url}" download target="_blank">Скачать обработанный файл</a>
         `;
     } catch (error) {
         // Обрабатываем ошибки
